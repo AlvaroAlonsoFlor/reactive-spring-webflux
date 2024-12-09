060396cf0f4a7ba8c26a599f286b5bacf9baed8e
Alvaro <alvaroalonsoflor@gmail.com>
2024-12-09 16:44:58 +0000

    test: add unit tests

--- movies-info-service/src/test/java/intg/com/reactivespring/controller/MoviesInfoIntegrationTest.java
+++ movies-info-service/src/test/java/intg/com/reactivespring/controller/MoviesInfoIntegrationTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @AutoConfigureWebTestClient
-class MoviesInfoControllerTest {
+class MoviesInfoIntegrationTest {
 
     @Autowired
     MovieInfoRepository movieInfoRepository;
