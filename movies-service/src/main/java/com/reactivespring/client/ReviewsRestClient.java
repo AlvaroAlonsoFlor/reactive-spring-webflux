70fc54a4e0c9cf84d84d8be65a49de2171002928
Alvaro <alvaroalonsoflor@gmail.com>
2024-12-30 16:29:51 +0000

    feat: add reviews client

---
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -0,0 +1,36 @@
+package com.reactivespring.client;
+
+import com.reactivespring.domain.Review;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Component
+public class ReviewsRestClient {
+
+    @Value("${restClient.reviewsUrl}")
+    private String reviewUrl;
+
+    private WebClient webClient;
+
+    public ReviewsRestClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Flux<Review> retrieveReviews(String movieId) throws URISyntaxException {
+        var url = UriComponentsBuilder.fromUri(new URI(reviewUrl))
+                .queryParam("movieId", movieId)
+                .buildAndExpand().toUriString();
+
+        return webClient
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToFlux(Review.class);
+    }
+}
