1962fdf918eddcd448f89c576f53375133fa1e38
Alvaro <alvaroalonsoflor@gmail.com>
2024-12-30 13:05:23 +0000

    test: add review integration scenario

---
+++ movies-review-service/src/test/java/intg/com/reactivespring/routes/ReviewsIntegrationTest.java
@@ -0,0 +1,69 @@
+package com.reactivespring.routes;
+
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureDataMongo
+@AutoConfigureWebTestClient
+public class ReviewsIntegrationTest {
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @Autowired
+    ReviewReactiveRepository repository;
+
+    static final String REVIEWS_URL = "/v1/reviews";
+
+    @BeforeEach
+    void setUp() {
+        var reviewsList = List.of(
+                new Review(null, 1L, "Awesome Movie", 9.0),
+                new Review(null, 1L, "Awesome Movie1", 9.0),
+                new Review(null, 2L, "Excellent Movie", 8.0)
+        );
+
+        repository.saveAll(reviewsList).blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        repository.deleteAll().block();
+    }
+
+    @Test
+    void addReview() {
+        var review = new Review(null, 1L, "Awesome Movie", 9.0);
+
+        webTestClient
+                .post()
+                .uri(REVIEWS_URL)
+                .bodyValue(review)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(Review.class)
+                .consumeWith(reviewEntityExchangeResult -> {
+                    var savedReview = reviewEntityExchangeResult.getResponseBody();
+                    assert savedReview != null;
+                    assertNotNull(savedReview.getReviewId());
+                });
+
+    }
+
+}
