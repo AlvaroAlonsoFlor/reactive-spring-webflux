d8682b5e7170dcb8d235641a12035f5b37013765
Alvaro <alvaroalonsoflor@gmail.com>
2025-01-20 16:17:19 +0000

    test: add happy path movie scenario

--- movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
+++ movies-service/src/test/java/intg/com/reactivespring/controller/MoviesControllerIntgTest.java
@@ -1,4 +1,58 @@
 package com.reactivespring.controller;
 
+import com.reactivespring.domain.Movie;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Objects;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+@AutoConfigureWireMock(port = 8084)
+@TestPropertySource(properties = {
+        "restClient.moviesInfoUrl=http://localhost:8084/v1/movies-info",
+        "restClient.reviewsUrl=http://localhost:8084/v1/reviews"
+})
 public class MoviesControllerIntgTest {
+
+    @Autowired
+    WebTestClient webClient;
+
+    @Test
+    public void retrieveMoviesById() {
+
+        var id = "abc";
+
+        stubFor(get(urlEqualTo(String.format("/v1/movies-info/%s", id)))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("movies-info.json")));
+
+        stubFor(get(urlPathEqualTo("/v1/reviews"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBodyFile("reviews.json")));
+
+        webClient
+                .get()
+                .uri("/v1/movies/{id}", id)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Movie.class)
+                .consumeWith(response -> {
+                    var movie = response.getResponseBody();
+                    assert Objects.requireNonNull(movie).getReviewList().size() == 2;
+                    assertEquals("Batman Begins", movie.getMovieInfo().getName());
+                });
+    }
 }
