ce282f80aa379dd2b6ef848629a03e8a9ec8596b
Alvaro <alvaroalonsoflor@gmail.com>
2025-01-20 14:22:57 +0000

    feat: handle MoviesInfoService upstream issues

--- movies-service/src/main/java/com/reactivespring/exceptionhandler/GlobalErrorHandler.java
+++ movies-service/src/main/java/com/reactivespring/exceptionhandler/GlobalErrorHandler.java
@@ -1,7 +1,9 @@
 package com.reactivespring.exceptionhandler;
 
 import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,5 +17,11 @@ public class GlobalErrorHandler {
         log.error("Exception caught in handleClientException: {}", exception.getMessage());
         return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
 
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleRuntimeException (RuntimeException exception) {
+        log.error("Exception caught in handleRuntimeException: {}", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
     }
 }
