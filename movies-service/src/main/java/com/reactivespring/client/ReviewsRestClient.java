f8f30e1e62b9273e6ec9d70726d77e4a67ce2f4f
Alvaro <alvaroalonsoflor@gmail.com>
2025-01-20 14:33:34 +0000

    feat: handle ReviewsService errors

--- movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/ReviewsRestClient.java
@@ -1,13 +1,20 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class ReviewsRestClient {
 
     @Value("${restClient.reviewsUrl}")
@@ -28,6 +35,21 @@ public class ReviewsRestClient {
                 .get()
                 .uri(url)
                 .retrieve()
+                .onStatus(HttpStatusCode::is4xxClientError, clientResponse -> {
+                    log.info("Status code is {}", clientResponse.statusCode().value());
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.empty();
+                    }
+
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new ReviewsClientException(responseMessage)));
+                })
+                .onStatus(HttpStatusCode::is5xxServerError, clientResponse -> {
+                    log.info("Status code is {}", clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new ReviewsServerException(
+                                    "Server Exception in ReviewsService " + responseMessage)));
+                })
                 .bodyToFlux(Review.class);
     }
 }
